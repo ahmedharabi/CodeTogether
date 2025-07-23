dcc514bc241d89a9848c317fe8620b850cee5970
ahmedharabi <ahmed.harabi@isimg.tn>
2025-07-23 21:04:56 +0100

    feat: add workspace system with create, join, and member management

---
+++ Backend/src/routes/workspace.route.ts
@@ -0,0 +1,12 @@
+import express, {RequestHandler} from "express";
+import {createWorkSpace,getWorkspaceById,updateWorkspace,deleteWorkspace,getWorkspacesForUser} from "../controllers/workspace.controller";
+
+const router=express.Router();
+
+router.get("/",getWorkspacesForUser as unknown as  RequestHandler);
+router.get("/:id",getWorkspaceById);
+router.post("/",createWorkSpace as unknown as  RequestHandler);
+router.put("/:id",updateWorkspace as unknown as  RequestHandler);
+router.delete("/:id",deleteWorkspace as unknown as  RequestHandler);
+
+export {router};
