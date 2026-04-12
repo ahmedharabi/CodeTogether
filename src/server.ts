f91b912e65ac72a10e399e934d16f759b590af52
ahmedharabi <harabiahmed88@gmail.com>
2026-04-12 22:34:56 +0100

    testing eks deploy

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import {app} from './app'
+import { app } from './app'
+import logger from './utils/logger'
 
-const PORT = 3002
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3002;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    logger.info(`Server is running on pt ${PORT}`);
 })
